c21c2fda7ddb2bd911389acf5955699593b1cdd3
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-05-21 21:36:30 +0530

    End Call Button

--- components/EndCallButton.tsx
+++ components/EndCallButton.tsx
@@ -1,5 +1,33 @@
+"use client";
+
+import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+
 const EndCallButton = () => {
-  return <div></div>;
+  const call = useCall();
+  const router = useRouter();
+
+  const { useLocalParticipant } = useCallStateHooks();
+  const localParticipant = useLocalParticipant();
+
+  const isMeetingOwner =
+    localParticipant &&
+    call?.state.createdBy &&
+    localParticipant.userId === call.state.createdBy.id;
+
+  if (!isMeetingOwner) return null;
+
+  const endCall = async () => {
+    await call.endCall();
+    router.push("/");
+  };
+
+  return (
+    <Button onClick={endCall} className="bg-red-500">
+      End call for everyone
+    </Button>
+  );
 };
 
 export default EndCallButton;
