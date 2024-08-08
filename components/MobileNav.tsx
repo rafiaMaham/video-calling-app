2e1aa325ea55cded1c59379171f3273027f24a40
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-08-08 21:19:56 +0530

    commit

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -35,7 +35,7 @@ const MobileNav = () => {
               alt="logo"
               className="max-sm:size-10"
             />
-            <p className="text-[26px] font-extrabold  text-white">BOOM</p>
+            <p className="text-[26px] font-extrabold  text-white">BLOOM</p>
           </Link>
 
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
